f7f48043f151b5aa068ef288b327626b7b3b241e
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-08-06 11:37:35 +0200

    Added simple-click example project

---
+++ examples/simple-click/src/js-scope.webapps_js-scope.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2015 Canonical Ltd.
+ *
+ * This file is part of unity-js-scopes.
+ *
+ * unity-js-scopes is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * unity-js-scopes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var scopes = require('unity-js-scopes')
+
+scopes.self.initialize(
+    {}
+    ,
+    {
+        run: function() {
+            console.log('Running...')
+        },
+        start: function(scope_id) {
+            console.log('Starting scope id: '
+                        + scope_id
+                        + ', '
+                        + scopes.self.scope_config)
+        },
+        search: function(canned_query, metadata) {
+            return scopes.lib.new_search_query(
+                canned_query,
+                metadata,
+                // run
+                function(search_reply) {
+                    var qs =
+                        canned_query.query_string();
+                    var category =
+                        search_reply.register_category("simple", "hello world from js", "");
+                    
+                    var categorised_result =
+                        scopes.lib.new_categorised_result(category);
+                    categorised_result.set_uri("http://www.ubuntu.com");
+                    categorised_result.set_title("'" + qs + "'");
+                    
+                    search_reply.push(categorised_result);
+                },
+                // cancelled
+                function() {
+                });
+        },
+        preview: function(result, action_metadata) {}
+    }
+);
+
