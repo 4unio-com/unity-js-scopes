b8148de4657257686bac304d7ffb0950cae9676e
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-09-29 09:18:33 +0200

    Made unity-js-scopes-tool a binary

---
+++ src/tool/main.cc
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2015 Canonical Ltd.
+ *
+ * This file is part of unity-js-scopes.
+ *
+ * unity-js-scopes is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * unity-js-scopes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "node.h"
+
+#include "../common/config.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
+
+void usage() {
+  std::cout << "Usage: "
+            << executable_name()
+            << " install "
+            << "<path/to/node_modules>"
+            << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    usage();
+    return EXIT_FAILURE;
+  }
+
+  if (std::string(argv[1]) == "install") {
+    if (argc != 3) {
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    std::string modules_dir = argv[2];
+
+    // Check that the target path is not empty
+    if (modules_dir.empty()) {
+      std::cout << "Target path can not be empty"
+                << std::endl;
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    // If the target path exists, check that it is a directory
+    if (boost::filesystem::exists(modules_dir)
+        && !boost::filesystem::is_directory(modules_dir)) {
+      std::cout << "'" << modules_dir << "' is not a directory"
+                << std::endl;
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    // Check that the target directory is named "node_modules"
+    if (!boost::algorithm::ends_with(modules_dir, "node_modules")
+        && !boost::algorithm::ends_with(modules_dir, "node_modules/")) {
+      std::cout << "Target directory should be named 'node_modules'"
+                << std::endl;
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    std::cout << "Installation started." << std::endl;
+
+    // Ensure that the target directory exists
+    if (!boost::filesystem::exists(modules_dir)) {
+      std::cout << "Creating '" << modules_dir << "' ..." << std::endl;
+      boost::filesystem::create_directories(modules_dir);
+    }
+
+    // Remove any existing unity-js-scopes bindings from the target directory
+    if (boost::filesystem::exists(modules_dir + "/unity-js-scopes")) {
+      std::cout << "Removing '" << modules_dir << "/unity-js-scopes' ..." << std::endl;
+      boost::filesystem::remove_all(modules_dir + "/unity-js-scopes");
+    }
+
+    // Copy the unity-js-scopes bindings into the target directory
+    std::cout << "Copying '/node_modules/unity-js-scopes' to '" << modules_dir << "' ..." << std::endl;
+    boost::filesystem::create_directory(modules_dir + "/unity-js-scopes");
+    boost::filesystem::copy("/node_modules/unity-js-scopes/index.js",
+                            modules_dir + "/unity-js-scopes/index.js");
+    boost::filesystem::copy("/node_modules/unity-js-scopes/unity_js_scopes_bindings.node",
+                            modules_dir + "/unity-js-scopes/unity_js_scopes_bindings.node");
+
+    boost::filesystem::create_directory(modules_dir + "/unity-js-scopes/lib");
+    boost::filesystem::copy("/node_modules/unity-js-scopes/lib/scope-core.js",
+                            modules_dir + "/unity-js-scopes/lib/scope-core.js");
+
+    std::cout << "Copying '/usr/bin/unity-js-scopes-launcher' to '" << modules_dir << "/unity-js-scopes/bin' ..." << std::endl;
+    boost::filesystem::create_directory(modules_dir + "/unity-js-scopes/bin");
+    boost::filesystem::copy("/usr/bin/unity-js-scopes-launcher",
+                            modules_dir + "/unity-js-scopes/bin/unity-js-scopes-launcher");
+
+    // Rebuild any binary npm modules for the current targeted arch
+    std::cout << "Rebuilding binary modules in '" << modules_dir << "' ..." << std::endl;
+    std::string npm_script = "/node_modules/npm/cli.js";
+    std::string prefix_arg = "--prefix='" + modules_dir + "/../'";
+    std::string rebuild_arg = "rebuild";
+
+    std::vector<std::string::value_type>
+        new_args_content(
+          executable_name().size() + 1
+          + npm_script.size() + 1
+          + prefix_arg.size() + 1
+          + rebuild_arg.size() + 1);
+
+    memcpy(&new_args_content[0],
+        executable_name().c_str(),
+        executable_name().size() + 1);
+
+    memcpy(&new_args_content[0]
+        + executable_name().size() + 1,
+        npm_script.c_str(),
+        npm_script.size() + 1);
+
+    memcpy(&new_args_content[0]
+        + executable_name().size() + 1
+        + npm_script.size() + 1,
+        prefix_arg.c_str(),
+        prefix_arg.size() + 1);
+
+    memcpy(&new_args_content[0]
+        + executable_name().size() + 1
+        + npm_script.size() + 1
+        + prefix_arg.size() + 1,
+        rebuild_arg.c_str(),
+        rebuild_arg.size() + 1);
+
+    std::vector<std::string::value_type*>
+        new_args;
+    new_args.push_back(&new_args_content[0]);
+    new_args.push_back(&new_args_content[0] + executable_name().size() + 1);
+    new_args.push_back(&new_args_content[0] + executable_name().size() + 1 + npm_script.size() + 1);
+    new_args.push_back(&new_args_content[0] + executable_name().size() + 1 + npm_script.size() + 1 + prefix_arg.size() + 1);
+
+    int result = node::Start(
+          new_args.size(),
+          &new_args[0]);
+
+    if (result == EXIT_SUCCESS)
+    {
+      std::cout << "Installation complete." << std::endl;
+    }
+    else
+    {
+      std::cout << "Installation failed." << std::endl;
+    }
+
+    return result;
+  }
+  else {
+    std::cout << "Running: \"node";
+    for (int i = 1; i < argc; ++i)
+    {
+      std::cout << " " << argv[i];
+    }
+    std::cout << "\" ..." << std::endl;
+
+    return node::Start(argc, argv);
+  }
+}
