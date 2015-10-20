7c745997250ef362c324832abce93a1ae9ca291a
Alexandre Abreu <alexandre.abreu@canonical.com>
2015-10-20 16:44:15 -0400

    Add docs

---
+++ doc/docbuild/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ActionMetadata",
+        "ActivationQuery",
+        "CategorisedResult",
+        "Category",
+        "CategoryRenderer",
+        "ColumnLayout",
+        "PreviewQuery",
+        "PreviewReply",
+        "PreviewWidget",
+        "Result",
+        "Scope",
+        "SearchMetaData",
+        "SearchQuery",
+        "SearchReply"
+    ],
+    "modules": [
+        "ScopeJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "ScopeJS",
+            "name": "ScopeJS",
+            "description": "Metadata passed to scopes for preview and activation"
+        }
+    ]
+} };
+});
