95926e0f15716ecd6a84816c8090af81ee6aa3b5
David Callé <davidc@framli.eu>
2016-01-07 17:12:30 +0100

    Update doc with make doc, to actually fill the data.json file, allowing doc extraction on the developer site

--- doc/docbuild/assets/js/api-filter.js
+++ doc/docbuild/assets/js/api-filter.js
@@ -40,6 +40,10 @@ Y.APIFilter = Y.Base.create('apiFilter', Y.Base, [Y.AutoCompleteBase], {
             }
         });
 
+        if (this.get('queryType') === 'elements') {
+            name = '&lt;' + name + '&gt;';
+        }
+
         return name;
     }
 
@@ -50,7 +54,7 @@ Y.APIFilter = Y.Base.create('apiFilter', Y.Base, [Y.AutoCompleteBase], {
             value: 'phraseMatch'
         },
 
-        // May be set to "classes" or "modules".
+        // May be set to "classes", "elements" or "modules".
         queryType: {
             value: 'classes'
         },
